1a3c40602c60ad9b95b2b68c9042b391c8d112ce
Julia4421 <90348347+Julia4421@users.noreply.github.com>
2023-11-22 17:45:25 +0100

    Update ksiazki.js
    poprawione 22.11

--- 3 zdalne zajecia termin 20.11/ksiazki.js
+++ 3 zdalne zajecia termin 20.11/ksiazki.js
@@ -1,34 +1,48 @@
 const books = [
-    {title: 'Total loss 100', pages: 600, genre: 'fantasy', rating: 7},
-    {title: 'Total enlightenment', pages: 250, genre: 'romance', rating: 4},
-    {title: 'Big loss', pages: 400, genre: 'fantasy', rating: 2},
-    {title: 'Tenth Joy', pages: 32, genre: 'action', rating: 9},
-    {title: 'Quickfix number 4', pages: 15, genre: 'fantasy', rating: 8},
-    {title: 'World Ender 3', pages: 199, genre: 'fantasy', rating: 1},
-    {title: 'Paranormal', pages: 200, genre: 'thriller', rating: 5},
+  {title: 'Total loss 100', pages: 600, genre: 'fantasy', rating: 7},
+  {title: 'Total enlightenment', pages: 250, genre: 'romance', rating: 4},
+  {title: 'Big loss', pages: 400, genre: 'fantasy', rating: 2},
+  {title: 'Tenth Joy', pages: 32, genre: 'action', rating: 9},
+  {title: 'Quickfix number 4', pages: 15, genre: 'fantasy', rating: 8},
+  {title: 'World Ender 3', pages: 199, genre: 'fantasy', rating: 1},
+  {title: 'Paranormal', pages: 200, genre: 'thriller', rating: 5},
 ];
+
+const compose = (...functions) => input =>
+  functions.reduceRight((acc, fn) => fn(acc), input);
 
 // Zadanie 6
 const litery = books =>
-  books
-    .filter(book => book.pages % 2 === 0 && book.genre.endsWith('y'))
-    .reduce((acc, book) => acc + book.title.replace(/ /g, '').length, 0);
+  compose(
+    books =>
+      books.reduce((acc, book) => acc + book.title.replace(/ /g, '').length, 0),
+    books =>
+      books.filter(book => book.pages % 2 === 0 && book.genre.endsWith('y'))
+  )(books);
 
 console.log('Ilość liter w tytule:', litery(books));
 
 // Zadanie 7
 const oceny = books =>
-  books
-    .filter(book => book.pages % 2 !== 0 && /\d/.test(book.title) && book.rating > 5)
-    .length;
+  compose(
+    books =>
+      books.filter(
+        book => book.pages % 2 !== 0 && /\d/.test(book.title) && book.rating > 5
+      ),
+    books => books.length
+  )(books);
 
 console.log('Pozytywnych ocen:', oceny(books));
 
 // Zadanie 8
 const tytul = books =>
-  books
-    .map(book => book.title)
-    .sort((a, b) => b.length - a.length)
-    .filter((title, index, array) => title.length < array[1].length)[0];
+  compose(
+    books =>
+      books
+        .map(book => book.title)
+        .sort((a, b) => b.length - a.length)
+        .filter((title, index, array) => title.length < array[1].length),
+    books => books[0]
+  )(books);
 
 console.log('Najdłuższy tytuł', tytul(books));
